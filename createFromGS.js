cfe76ce6068c4a50a4143ac1254c0ffd0ae600ea
jollyren <jollyren@gmail.com>
2023-08-09 14:18:51 -0600

    added Sam's cohorts and ITMs, added over absences email message, edited conditionals, created test email message logs

--- createFromGS.js
+++ createFromGS.js
@@ -2,28 +2,14 @@ require('dotenv').config()
 // const express = require('express')
 const {
   getToken,
+  getGP,
   getDFile,
   getDataGS,
-  getDataGSNew,
-  getGP,
-  makeRecordLoop,
-  makeRecordLoopNew,
-  appendLoop,
-  appendLoopNew,
-  getDataGSTest
+  filterAndFormatEntries,
+  generateEmails
 } = require('./utils.js')
 
-const gsDate = process.env.GS_DATE || ''
-const amas = process.env.AMAS || 'jeremy'
-
-// const app = express()
-// const PORT = process.env.PORT
-const cohorts = new Set(["2302-ACC-ET-WEB-PT-C", "2302-ACC-ET-WEB-PT-D", "2302-ACC-PT-WEB-PT-C", "2302-ACC-PT-WEB-PT-D", "2302-ACC-PT-WEB-PT-E", "2302-ACC-MT-CYB-PT-A"])
-console.log(cohorts)
-
 const main = async () => {
-  if (!gsDate) return console.log('No Date!')
-
   const token = await getToken()
   const user = await getGP(token)
   // const me = user.emailAddress.slice(0, user.emailAddress.indexOf('@'))
@@ -32,7 +18,7 @@ const main = async () => {
   // grab file and get data - change to desired week's date in env
   console.log('getting file!')
 
-  const file = await getDFile(gsDate)
+  const file = await getDFile()
   if (file)
     console.log(`
   kind: ${file.kind}
@@ -42,48 +28,18 @@ const main = async () => {
   // exit early if no file found
   else return console.log('NO FILE FOUND!')
 
-  const cybFile = await getDFile(gsDate, false)
-  if (cybFile)
-    console.log(`
-kind: ${cybFile.kind}
-id: ${cybFile.id}
-name: ${cybFile.name}
-`)
-  else return console.log('NO CYBER FILE FOUND!')
-
   // create query object
-  const data = [getDataGSNew({
-      spreadsheetId: file.id,
-      sheetName: "attendanceSheet",
-      firstCol: 'A',
-      lastCol: 'J'
-    }, true, cohorts)]
-  // console.log(data)
-  data.push(
-    getDataGSNew(
-      {
-        spreadsheetId: cybFile.id,
-        sheetName: gsDate.slice(0, gsDate.lastIndexOf('-')),
-        firstCol: 'A',
-        lastCol: 'I'
-      },
-      false, cohorts
-    )
-  )
-  // take promises and convert to entries, then flatten
-  const entriesArray = await Promise.all(data)
-  // console.log(entriesArray)
-  const entries = entriesArray.flat(1)
+  const data = await getDataGS({
+    spreadsheetId: file.id,
+    sheetName: 'attendanceSheet',
+    firstCol: 'A',
+    lastCol: 'BI'
+  })
 
-  if (entries.length) console.log('\nFirst List\n', entries[0])
-  // let isFirst = true
-  // entries.forEach((entry, i, arr) => {
-  //   if (i === 0) console.log(entry)
-  //   if (entry.cohort === '2302-ACC-MT-CYB-PT-A' && isFirst) console.log(entry), (isFirst = false)
-  // })
-  await makeRecordLoopNew(entries)
-
-  appendLoopNew(entries, cohorts)
+  const entries = data.reduce(filterAndFormatEntries, [])
+  console.log(data.length, entries.length)
+  console.log(entries[0])
+  await generateEmails(entries)
 }
 
 main()
--- utils.js
+++ utils.js
@@ -1,266 +1,281 @@
 const { google } = require('googleapis')
-const { writeFileSync, appendFileSync } = require('fs')
-const { readFile } = require('fs').promises
-const crypto = require('node:crypto')
-
-const gsDate = process.env.GS_DATE
 
 const oAuth2Client = new google.auth.OAuth2(
-	process.env.CLIENT_ID,
-	process.env.CLIENT_SECRET,
-	process.env.REDIRECT_URI
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URI
 )
 
 oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN })
 
+const svcSheets = google.sheets({ version: 'v4', auth: oAuth2Client })
+const svcDrive = google.drive({ version: 'v3', auth: oAuth2Client })
+const gmail = google.gmail({ version: 'v1', auth: oAuth2Client })
+
 // google helpers
 const getConfig = (method, token) => {
-	return {
-		method,
-		headers: {
-			Authorization: `Bearer ${token}`,
-			'Content-Type': 'application/json',
-		},
-	}
+  return {
+    method,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    }
+  }
 }
 
 const getToken = async () => {
-	const { token } = await oAuth2Client.getAccessToken()
-	// console.log(token)
-	return token
+  const { token } = await oAuth2Client.getAccessToken()
+  // console.log(token)
+  return token
 }
 
 const getGP = async (token) => {
-	try {
-		const url = 'https://gmail.googleapis.com/gmail/v1/users/me/profile'
+  try {
+    const url = 'https://gmail.googleapis.com/gmail/v1/users/me/profile'
 
-		const res = await fetch(url, {
-			method: 'GET',
-			headers: {
-				Authorization: `Bearer ${token}`,
-				'Content-Type': 'application/json',
-			},
-		})
-		if (res.status > 200) {
-			throw console.error(`${res.status}: ${res.statusText}`)
-		}
-		if (res.ok) {
-			const pdata = await res.json()
-			if (pdata.error) {
-				throw console.error(pdata.error)
-			}
-			return pdata
-		}
-	} catch (error) {
-		console.error(error)
-	}
+    const res = await fetch(url, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      }
+    })
+    if (res.status > 200) {
+      throw console.error(`${res.status}: ${res.statusText}`)
+    }
+    if (res.ok) {
+      const pdata = await res.json()
+      if (pdata.error) {
+        throw console.error(pdata.error)
+      }
+      return pdata
+    }
+  } catch (error) {
+    console.error(error)
+  }
 }
 
-const getDFile = async (date, isWeb = true) => {
-	const svcDrive = google.drive({ version: 'v3', auth: oAuth2Client })
-	try {
-		const searchString = isWeb ? `U-ATT-${date}` : '2302-ACC-MT-CYB-PT-A'
-		const {
-			data: {
-				files: [file],
-			},
-		} = await svcDrive.files.list({
-			q: `name contains '${searchString}'`,
-			spaces: 'drive',
-		})
+const getDFile = async () => {
+  try {
+    const searchString = '2302-full-attendance'
+    const {
+      data: {
+        files: [file]
+      }
+    } = await svcDrive.files.list({
+      q: `name contains '${searchString}'`,
+      spaces: 'drive'
+    })
 
-		return file ? file : console.log('No file!')
-	} catch (error) {
-		console.error(error)
-	}
+    return file ? file : console.log('No file!')
+  } catch (error) {
+    console.error(error)
+  }
 }
 
-const getDataGSNew = async (ssQuery, isWeb = true, cohorts) => {
-	// console.log(ssQuery)
+const getDataGS = async (ssQuery) => {
+  // console.log(ssQuery)
 
-	const { spreadsheetId, sheetName, firstCol, lastCol } = ssQuery
+  const { spreadsheetId, sheetName, firstCol, lastCol } = ssQuery
 
-	const svcSheets = google.sheets({ version: 'v4', auth: oAuth2Client })
-	const range = `${sheetName}!${firstCol}:${lastCol}`
+  const range = `${sheetName}!${firstCol}:${lastCol}`
 
-	const {
-		data: { values },
-	} = await svcSheets.spreadsheets.values.get({
-		spreadsheetId,
-		range,
-	})
+  const {
+    data: { values }
+  } = await svcSheets.spreadsheets.values.get({
+    spreadsheetId,
+    range
+  })
 
-	let valuesLength = 0
+  let valuesLength = 0
 
-	//create entries, skip blanks
+  //create entries, skip blanks
 
-	const entries = values.reduce((a, c, i) => {
-		const keys = values[0]
-		if (i === 0 || !c[0]) return a
+  const entries = values.reduce((a, c, i) => {
+    const keys = values[0]
+    if (i === 0 || !c[0]) return a
+    const entry = {}
+    keys.forEach((key, idx) => (entry[key] = c[idx]))
 
-		const cohort = !isWeb ? '2302-ACC-MT-CYB-PT-A' : null
-		const entry = { cohort }
-		keys.forEach((key, idx) => (entry[key] = c[idx]))
+    a.push(entry)
+    valuesLength++
+    return a
+  }, [])
+  if (valuesLength) console.log(`got ${valuesLength} values!`)
 
-		a.push(entry)
-		valuesLength++
-		return a
-	}, [])
-	if (valuesLength) console.log(`got ${valuesLength} values!`)
+  return entries.length
+    ? (console.log(`got ${entries.length} entries!`), entries)
+    : (console.log('no entries to send along!'), [])
+}
 
-	return entries.length
-		? (console.log(`got ${entries.length} entries!`), entries)
-		: (console.log('no entries to send along!'), [])
+const filterAndFormatEntries = (listOfEntries, entry) => {
+  const isWithdrawn = entry.isXferToAug || entry.isWithdrawn
+  if (!isWithdrawn) {
+    // console.log(entry.sName)
+    const newObj = {
+      sName: entry.sName,
+      email: entry.emails,
+      cohort: entry.cohort,
+      absences: isNaN(Number(entry.absencePlusPartial)) ? 0 : Number(entry.absencePlusPartial),
+      excusedCount: isNaN(Number(entry.excusedAbsences)) ? 0 : Number(entry.excusedAbsences),
+      attended: [],
+      absent: [],
+      partial: [],
+      excused: []
+    }
+    for (const key in entry) {
+      if (key.indexOf('/') > 0 && Date.parse(key) < Date.now()) {
+        if (entry[key] === 'Attended') newObj.attended.push(key)
+        if (entry[key] === 'Absent') newObj.absent.push(key)
+        if (entry[key] === 'Partial') newObj.partial.push(key)
+        if (entry[key] === 'Excused') newObj.excused.push(key)
+      }
+    }
+    listOfEntries.push(newObj)
+  }
+  return listOfEntries
+}
+//, entry[]
+
+const createDraft = async (raw, gmail) => {
+  try {
+    const res = await gmail.users.drafts.create({
+      userId: 'me',
+      requestBody: { message: { raw } }
+    })
+    console.log(`Draft created with ID: ${res.data.id}`)
+  } catch ({ errors }) {
+    errors.forEach((err) =>
+      console.error('Error creating draft:', err.message, err.domain, err.reason)
+    )
+  }
+}
+
+const generateEmails = async (entries) => {
+  const domain = '@fullstackacademy.com'
+  const itmEmails = {
+    '2302-ACC-ET-WEB-PT-A': `<>, <michael.pascuzzi${domain}>, <Frank.Chambergo${domain}>, <Thomas.Pollick${domain}>`,
+    '2302-ACC-ET-WEB-PT-B': `<charles.lucas${domain}>, <Aiesha.Brown${domain}>, <Woody.lucas${domain}>`,
+    '2302-ACC-ET-WEB-PT-C': `<jeffrey.yen${domain}>, <shaquon.kelley${domain}>, <joey.pedicini${domain}>`,
+    '2302-ACC-ET-WEB-PT-D': `<bianca.dennis${domain}>, <philip.lemaster${domain}>, <Phillip.Shim${domain}>`,
+    '2302-ACC-ET-WEB-PT-E': `<tony.shaker${domain}>, <Abi.scholz${domain}>, <Jing.cao${domain}>`,
+    '2302-ACC-CT-WEB-PT-A': `<Aaron.shen${domain}>, <Gonca.ay${domain}, <Kemp.atkinson${domain}>`,
+    '2302-ACC-CT-WEB-PT-B': `<jacqueline.levine${domain}>, <Caitlin.lamprecht${domain}>, <Elijah.hensel${domain}>`,
+    '2302-ACC-PT-WEB-PT-A': `<John.sickels${domain}>, <Abidur.dipta${domain}>, <Jonathan.martinez${domain}>`,
+    '2302-ACC-PT-WEB-PT-B': `<Aaron.katz${domain}>, <Danielle.williams${domain}, <David.boicourt${domain}>`,
+    '2302-ACC-PT-WEB-PT-C': `<kelsey.schroeder${domain}>, <Esteban.Ordonez-Benavides${domain}>, <shelby.robinson${domain}>`,
+    '2302-ACC-PT-WEB-PT-D': `<Alia.Abdulahi${domain}>, <Xavier.LoeraFlores${domain}>, <Tyler.Lemke${domain}>`,
+    '2302-ACC-PT-WEB-PT-E': `<david.liang${domain}>, <ronnie.rios${domain}>, <wallie.raihan${domain}>`
+  }
+  const itmList = {
+    '2302-ACC-ET-WEB-PT-A': 'Sam, Michael, Frank, or Thomas',
+    '2302-ACC-ET-WEB-PT-B': 'Sam, Chuck, Aiesha, or Woody',
+    '2302-ACC-ET-WEB-PT-C': 'Jeff, Shaquon, or Joey',
+    '2302-ACC-ET-WEB-PT-D': 'Bianca, Philip, or Phillip',
+    '2302-ACC-ET-WEB-PT-E': 'Sam, Tony, Abi, or Jing',
+    '2302-ACC-CT-WEB-PT-A': 'Sam, Aaron, Gonca, or Kemp',
+    '2302-ACC-CT-WEB-PT-B': 'Sam, Jackie, Caitlin, or Elijah',
+    '2302-ACC-PT-WEB-PT-A': 'Sam, John, Abidur, or Jonathan',
+    '2302-ACC-PT-WEB-PT-B': 'Sam, Aaron, Danielle, or David',
+    '2302-ACC-PT-WEB-PT-C': 'Kelsey, Esteban, or Shelby',
+    '2302-ACC-PT-WEB-PT-D': 'Tyler, Alia, or Xavier',
+    '2302-ACC-PT-WEB-PT-E': 'David, Wayel, or Ronnie'
+  }
+
+  const absenceWords = [
+    'offset',
+    'One',
+    'Two',
+    'Three',
+    'Four',
+    'Five',
+    'Six',
+    'Seven',
+    'Eight',
+    'Nine',
+    'Ten'
+  ]
+
+  let emails = 0
+  let overAbsenceTest = undefined
+  let normalAbsenceTest = undefined
+
+  for (const entry of entries) {
+    const len = entry.absent.length
+    const tenDaysAgo = Date.now() - 1000 * 60 * 60 * 24 * 10
+    const lastAbsence = Date.parse(entry.absent[len - 1])
+    if (!entry.absences || isNaN(lastAbsence)) continue
+    // console.log('qualifies - has absences')
+    const { sName, email, cohort, absences, excusedCount, attended, absent, partial, excused } =
+      entry
+
+    const absenceRem = 10 - absences
+    const isRecent = !(lastAbsence < tenDaysAgo)
+    const isOverAbsences = absenceRem < 0
+    if (isOverAbsences) {
+      // console.log(`creating overAbsences email to ${sName}`)
+      // console.log(absences)
+      // console.log(absent.at(-1))
+      // over absences allottment
+      const message = [
+        'Content-Type: text/plain; charset="UTF-8"\n',
+        'MIME-Version: 1.0\n',
+        'Content-Transfer-Encoding: 7bit\n',
+        `to: <${email}>\n`,
+        `cc: ${itmEmails[cohort]}\n`,
+        `from: "Jeremy Rogers" <jeremy.rogers@fullstackacademy.com>\n`,
+        `subject: FSA x ACC - Web Dev - Absences Exceeded [[ACTION REQUIRED]]\n\n`,
+        `Hello ${sName},\n\nI hope you're doing well. I noticed that you've been out out these dates${absent.join(
+          '\n'
+        )}.\n\n${
+          partial.length ? 'You also have parial days of ' + partial.join(', ') + '\n\n' : ''
+        }Please be aware you have exceeded your allotted absences and partials and may be withdrawn under the rules of the SEA you signed and the various times it's been mentioned during the course.\n\nIf you feel any of these absences are incorrect, please contact me to make the appropriate corrections. Thank you!\n\nHope to hear back soon!`
+      ].join('')
+      if (!overAbsenceTest) overAbsenceTest = '-------\n\n' + message
+      const raw = Buffer.from(message).toString('base64')
+      // await createDraft(raw, gmail)
+      emails++
+      // console.log(`isOverAbsences email to ${sName} created`)
+    } else if (isRecent) {
+      // console.log(`creating normal absences email to ${sName}`)
+      // console.log(absences, absent.at(-1))
+      // normal message about absences
+      const message = [
+        'Content-Type: text/plain; charset="UTF-8"\n',
+        'MIME-Version: 1.0\n',
+        'Content-Transfer-Encoding: 7bit\n',
+        `to: <${email}>\n`,
+        `cc: ${itmEmails[cohort]}\n`,
+        `from: "Jeremy Rogers" <jeremy.rogers@fullstackacademy.com>\n`,
+        `subject: FSA x ACC - Web Dev - ${absenceWords[absences]} or More Absences\n\n`,
+        `Hello ${sName},\n\nI hope you're doing well. I noticed that you were out ${absent.join(
+          ', '
+        )}.\n\n${
+          partial.length ? 'You also have parial days of ' + partial.join(', ') + '\n\n' : ''
+        }Please let your instructional team know if you need extra assistance in regard to catching up or feeling comfortable with any concepts you might feel shaky on.\n\nPlease be aware you have ${absenceRem} left.\n\nDon't hesitate to reply back any time to me, ${
+          itmList[cohort]
+        }.\n\nWe're here to support you to the best of our ability. Please use us as a resource! 🙂\n\nHope to hear back soon!`
+      ].join('')
+      if (!normalAbsenceTest) normalAbsenceTest = '-------\n\n' + message
+      const raw = Buffer.from(message).toString('base64')
+      // await createDraft(raw, gmail)
+      emails++
+      // console.log(`Normal Absence email to ${sName} created`)
+    }
+  }
+  console.log(`generating ${emails} emails`)
+  console.log(overAbsenceTest ? overAbsenceTest : undefined)
+  console.log(normalAbsenceTest ? normalAbsenceTest : undefined)
 }
 
 // helpers
-const checkFileAsync = async (filePath) => {
-	try {
-		const data = await readFile(filePath, 'utf8', { flags: 'rx' })
-		return data
-	} catch (error) {
-		if (error.code === 'ENOENT') {
-			console.error('File not found')
-		}
-		// console.error('Error reading file: ', error)
-	}
-}
-const appending = async (path, data) => {
-	appendFileSync(path, data)
-}
-const handleError = (e) => {
-	console.error('CSV file has not been created!\n', e)
-}
-const generateHash = (str) => {
-	return crypto.createHash('md5').update(str).digest('hex')
-}
-
-const makeCsvRecord = async ({ classDay, data }) => {
-	const filePath = `${classDay ? 'classes' : 'allRecords'}-${gsDate}.csv`
-	const headers = classDay ? 'sName,status,date,cohort\n' : `${Object.keys(data).join(',')}\n`
-
-	const fileTest = await checkFileAsync(filePath)
-	if (!fileTest) {
-		console.log('Writing New File')
-		writeFileSync(filePath, headers, handleError)
-	}
-	console.log('Checking if new file exists...')
-	const fileTestTwo = await checkFileAsync(filePath)
-	fileTestTwo ? console.log(true) : console.log(false)
-
-	return fileTestTwo
-}
-
-const makeRecordLoopNew = async (entries) => {
-	;(await makeCsvRecord({
-		classDay: 'class1',
-		data: { sName: 'sName', c1Status: 'c1Status', date: 'date', cohort: 'cohort' },
-	}))
-		? console.log('writing c1 import file successful!')
-		: console.log('c1 aready existed')
-	;(await makeCsvRecord({
-		classDay: 'class2',
-		data: { sName: 'sName', c2Status: 'c2Status', date: 'date', cohort: 'cohort' },
-	}))
-		? console.log('writing c2 import file successful!')
-		: console.log('c2 aready existed')
-}
-
-// createFromGS.js append records
-const appendAllRecordCSV = async ({ classDay, data }) => {
-	const filePath = `${classDay ? 'classes' : 'allRecords'}-${gsDate}.csv`
-	const dataStr = Object.values(data).join(',') + '\n'
-	//get current file data
-	const oneHash = generateHash(await checkFileAsync(filePath))
-	//append data
-	await appending(filePath, dataStr)
-	//get file data after append
-	const twoHash = generateHash(await checkFileAsync(filePath))
-	// return the comparison of hashes
-	return oneHash !== twoHash
-}
-
-// appending loop below
-const dateMapCB = (n, i) => (i == 1 ? Number(n) + 3 : Number(n))
-const getDate2 = (date) => date.split('-').map(dateMapCB).join('-')
-
-const appendLoopNew = async (entries, cohorts) => {
-	console.log('about to append')
-	entries.forEach(async (entry, i) => {
-		//exit loop early if not in cohort list
-		if (i == entries.length - 1) console.log('Ending Appending')
-		if (!cohorts.has(entry.cohort)) return
-		const c1 = gsDate
-		const c2 = getDate2(gsDate)
-
-		const { cohort, sName, class1: c1s, class2: c2s } = entry
-		const absRecC1 = {
-			sName,
-			c1Status: c1s,
-			date: c1,
-			cohort,
-		}
-		const absRecC2 = {
-			sName,
-			c2Status: c2s,
-			date: c2,
-			cohort,
-		}
-
-		const c1Match = c1s !== 'Attended' && c1s !== 'No Status' && c1s !== ''
-		const c2Match = c2s !== 'Attended' && c2s !== 'No Status' && c2s !== ''
-		if (c1Match) {
-			;(await appendAllRecordCSV({ classDay: 'class1', data: absRecC1 }))
-				? console.log('Appended class 1 records')
-				: console.log('did not append')
-		}
-		if (c2Match) {
-			;(await appendAllRecordCSV({ classDay: 'class2', data: absRecC2 }))
-				? console.log('Appended class 2 records')
-				: console.log('did not append')
-		}
-	})
-}
-
-// Testing for cyber only
-async function getDataGSTest() {
-	const cybFile = await getDFile(gsDate, false)
-	if (cybFile)
-		console.log(`
-kind: ${cybFile.kind}
-id: ${cybFile.id}
-name: ${cybFile.name}
-`)
-	if (!cybFile) return console.log('NO CYBER FILE FOUND!')
-	const date = gsDate.slice(0, gsDate.lastIndexOf('-'))
-	const testPromise = getDataGS(
-		{
-			spreadsheetId: cybFile.id,
-			sheetName: date,
-			firstCol: 'A',
-			lastCol: 'I',
-		},
-		false
-	)
-	console.log(testPromise)
-	const testEntry = await Promise.all([testPromise])
-	const entries = testEntry.flat(1)
-
-	console.log(entries)
-	await makeRecordLoop(entries)
-	appendLoop(entries)
-}
 
 module.exports = {
-	getConfig,
-	getToken,
-	getGP,
-	getDFile,
-	getDataGSNew,
-	appendAllRecordCSV,
-	makeCsvRecord,
-	getDate2,
-	appendLoopNew,
-	makeRecordLoopNew,
-	getDataGSTest,
+  getConfig,
+  getToken,
+  getGP,
+  getDFile,
+  getDataGS,
+  filterAndFormatEntries,
+  generateEmails
 }
